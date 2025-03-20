019e804275e64a522293ccd2a65b7a5f091e3c77
Hunter Sinclair <hsinclair@thinktiv.com>
2025-03-20 17:35:11 -0500

    move files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', 'localhost:3001']
